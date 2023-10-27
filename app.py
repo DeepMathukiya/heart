520db9925de052b312432731d10f42c044f27260
DeepMathukiya <dsmathukiya2004@gmail.com>
2023-10-27 20:47:24 +0530

    Change in app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-# from joblib import load
+from joblib import load
 import numpy as np
 import pickle
 # RF = pickle.load(open('Heart-disease-RF.pkl','rb'))
 
-# RF = load("HeartDisease-RF.joblib")
+RF = load("HeartDisease-RF.joblib")
 app = Flask(__name__)
 
 @app.route('/')
